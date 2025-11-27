9c8ae52a3f3f99346ef32434579a1e022e6ed224
Abhijitam01 <abhijitam01@gmail.com>
2025-11-27 12:10:16 +0530

    feat(user): add user auth page

---
+++ src/app/(samagri-user)/auth/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import AuthPage from '@/components/layout/user-login'
+import React from 'react'
+
+function LoginPage() {
+  return (
+    <div>
+        <AuthPage />
+    </div>
+  )
+}
+
+export default LoginPage
